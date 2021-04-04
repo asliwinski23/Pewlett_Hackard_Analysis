808b9f7f5d49ad13251503caeee6914f928eeb8b
Alaina Sliwinski <asliwinski23@gmail.com>
2021-04-04 19:20:24 -0400

    created retirement table and csv

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -50,3 +50,12 @@ CREATE TABLE dept_emp (
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
+
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
