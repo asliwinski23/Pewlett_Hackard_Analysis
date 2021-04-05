2e89a79a1d393fd3a98365ea85a8d17a06c9ecf1
Alaina Sliwinski <asliwinski23@gmail.com>
2021-04-04 22:16:16 -0400

    need to redo mentorship eligibility table

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,3 +17,24 @@ title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
+
+
+-- Challenge Deliverable 1 -- retiring_titles
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+-- Challenge Deliverable 2 -- mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+de.from_date, de.to_date, 
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no;
